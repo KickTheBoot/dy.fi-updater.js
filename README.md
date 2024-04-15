ca204ae0e69c107241caf86dcd3808877e00fd78
KickTheBoot <61482560+KickTheBoot@users.noreply.github.com>
2024-04-15 17:40:13 +0300

    Make the application a function for increased versatility

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
 # Dy.fi Updater.js
 This is a Node.js based application for updating dy.fi domains
-
-## config.json
-Here are the properties that config.json supports  
-**YOU SHOULD NOT INCLUDE THE CONFIG IN ANY OF YOUR COMMITS AS IT CONTAINS SENSITIVE INFORMATION LIKE YOUR USERNAME, PASSWORD AND DOMAIN**
-### Hostname
-This property tells the application the hostname you wish to update
-### UpdateInterval
-This property tells the application the time interval (in days) to send an update request to Dy.fi
-### EnvCreds
-When EnvCreds is set to true, the application will use the credentials stored in the DYFI_USER and DYFI_KEY environment variables to authorize the update
-### User, Key
-When EnvCreds is set to false, the application uses thest credentials to authorize the update
--- dyfi-updater.js
+++ dyfi-updater.js
@@ -1,40 +1,19 @@
 //requires here
 const http = require('http');
-const fs = require('fs');
+const package = require('./package.json');
 
-const config = require('./config.json');
-
-if(config.EnvCreds === true)  var Auth = `${process.env.DYFI_USER}:${process.env.DYFI_KEY}`;
-else
+exports.sendUpdateRequest = function(hostname, user, key)
 {
-    try
-    {
-        var Auth = `${config.User}:${config.Key}`;
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
-}
- //Set this value to the interval you want the requests to be sent (in days)
-var UpdateInterval = config.UpdateInterval;
-
-var options  = {
-    headers: {
-                "Authorization": `Basic ${btoa(Auth)}`,
-                "User-Agent": "dy.fi_updater.js/1.0.0 (anoojjonttu@gmail.com)",
-                "Cache-Control": "no-cache"
-            }
-}
-
-//run the update request once
-sendUpdateRequest()
-//routinely send a dns update request to dy.fi to make sure it doesn't expire
-setInterval(sendUpdateRequest, UpdateInterval * 24 * 3600 * 100);
-
-function sendUpdateRequest()
-{
-    http.get(`http://dy.fi/nic/update?hostname=${config.hostname}`, options, res => {
+    var Auth = `${user}:${key}`;
+    //construct the options object used for authentication
+    let options = {
+      headers: {
+        Authorization: `Basic ${btoa(Auth)}`,
+        "User-Agent": `${package.name}/${package.version} (anoojjonttu@gmail.com)`,
+        "Cache-Control": "no-cache",
+      }
+    };
+    http.get(`http://dy.fi/nic/update?hostname=${hostname}`, options, res => {
         let data = [];
         const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
         console.log('Status:', res.statusCode);
@@ -43,9 +22,6 @@ function sendUpdateRequest()
         res.on('data', (chunk) => {
             console.log('Response:', chunk);
         });
-        res.on('end', () => {
-            console.log('Response ended:', data);
-            });
     }).on('error', err => {
         console.log('Error:', err.message);
         });
